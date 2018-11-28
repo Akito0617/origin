95cee3ff0944f3a9a78dfc6b7f39290260d8c609
Takada Akito <>
2018-11-28 18:56:05 +0900

    Initial Commit

---
+++ tanni/Assets/test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+public class test : MonoBehaviour {
+
+	public void OnClick(){
+        //naichilab.UnityRoomTweet.Tweet ("ゲームID", "人望を"+(スコア).ToString()+"獲得しました", "unityroom");
+        naichilab.UnityRoomTweet.Tweet("KGJ20181104", ScoreManager.rank + "人望を" + MainManager.score.ToString() + "獲得しました(" + MainManager.selectGameMode + ")" , "unityroom");
+	}
+}
